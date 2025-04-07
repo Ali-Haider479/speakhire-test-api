f2047e86a8e003a347d8363962ca95c1cc0610e5
Muhammad Ibrar <ibrarsh001@gmail.com>
2025-04-07 13:24:37 +0500

    azure storage configuration done

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,11 +1,11 @@
-module.exports = () => ({
+module.exports = (env) => ({
   email: {
     config: {
       provider: "nodemailer",
       providerOptions: {
         host: process.env.SMTP_HOST,
-        port: process.env.SMTP_PORT,
-        secure: false,
+        port: parseInt(process.env.SMTP_PORT, 10), // Ensure it's a number
+        secure: false, // true if using port 465 (SSL), false for 587 (TLS)
         auth: {
           user: process.env.SMTP_USER,
           pass: process.env.SMTP_PASS,
@@ -14,6 +14,27 @@ module.exports = () => ({
       settings: {
         defaultFrom: process.env.SMTP_USER,
         defaultReplyTo: process.env.SMTP_USER,
+      },
+    },
+  },
+
+  upload: {
+    config: {
+      provider: "strapi-provider-upload-azure-storage",
+      providerOptions: {
+        authType: 'default',
+        account: process.env.AZURE_STORAGE_ACCOUNT,
+        accountKey: process.env.AZURE_STORAGE_ACCOUNT_KEY,
+        serviceBaseURL: process.env.AZURE_STORAGE_URL || undefined,
+        containerName: process.env.AZURE_STORAGE_CONTAINER_NAME,
+        defaultPath: "Assets",
+        cdnBaseURL: null
+      },
+      actionOptions: {
+        upload: {
+          // 👇 enables thumbnail generation
+          responsiveDimensions: true,
+        },
       },
     },
   },
