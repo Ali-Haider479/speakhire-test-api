0d1216f0d3eed1808e658b3efbd4b9af3a0b665f
Muhammad Ibrar <ibrarsh001@gmail.com>
2025-04-11 18:05:21 +0500

    image preview fix

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -9,4 +9,17 @@ export default [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', process.env.AZURE_STORAGE_URL],
+          'media-src': ["'self'", 'data:', 'blob:', process.env.AZURE_STORAGE_URL],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
 ];
